6fe9d9efa3a9de55515df49a2c0f9bff4db246be
Tord <tordsk@stud.ntnu.no>
2016-09-15 17:18:36 +0200

    support for multiple channels, messages are produced with the channel as topic, and spark produces result under the topic channel_result

--- src/streaming.scala
+++ src/streaming.scala
@@ -38,17 +38,74 @@ object streaming {
 
     ssc.checkpoint("checkpoint")
     val memes = ssc.sparkContext.textFile("memes")
-    val simplememes = memes.map(s=>s.toLowerCase()).collect()
+    val simplememes = memes.map(s => s.toLowerCase()).collect()
     val memesbrodc = ssc.sparkContext.broadcast(simplememes.toSet)
-    val topicmap = Map(("twitch_message",1))
-    val lines = KafkaUtils.createStream(ssc,"localhost","spark",topicmap)
-    val message = lines.map(_._2)
-    val words = message.flatMap(s=>s.split(" "))
+    val topics = ssc.sparkContext.textFile("channels")
+    val topicbrodc = ssc.sparkContext.broadcast(topics.map(s => (s, 1)).collect().toMap)
+    val zkQuorum = "localhost:2181"
+    val group = "client"
+    print(topicbrodc.value)
+    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicbrodc.value).window(Minutes(1), Seconds(5))
 
+
+    val message = lines.map(s => (s._1, s._2.toLowerCase()))
+    val memelines = message.filter(msg => msg._2.split(" ").toSet.intersect(memesbrodc.value).nonEmpty)
+    val totalMemesPrChannel = memelines.map(s => (s._1,1))
+      .reduceByKey((c,c1) => c + c1)
+    totalMemesPrChannel.print()
+
+    totalMemesPrChannel.foreachRDD(rdd =>{
+      rdd.foreachPartition(partition => {
+        val producer = new KafkaProducer[String, String](props)
+        partition.foreach(record => {
+          val message = new ProducerRecord[String, String](record._1.substring(1) + "_results", null, "TotalMemes,"+record._2)
+          producer.send(message)
+        })
+        producer.close()
+      })
+    })
+
+    message.map(t=> (t._1, 1))
+        .reduceByKey((i,i1) => i+i1)
+          .foreachRDD(rdd =>{
+            rdd.foreachPartition(partition => {
+              val producer = new KafkaProducer[String, String](props)
+              partition.foreach(record => {
+                val message = new ProducerRecord[String, String](record._1.substring(1) + "_results", null, "TotalMsg,"+record._2)
+                producer.send(message)
+              })
+              producer.close()
+            })
+          })
+
+      val memespresent = memelines.flatMap(s=> {
+        val memeset = s._2.split(" ").toSet.intersect(memesbrodc.value)
+        memeset.map(a=> s._1 ->a).toMap})
+      .map(s=>(s,1))
+      .reduceByKey((i,i1) => i + i1)
+      val topmemeprchannel = memespresent.map(s=>(s._1._1,(s._1._2,s._2)))
+      .reduceByKey((t,t1) => {
+        if (t._2 > t1._2)
+           t
+
+        else
+           t1
+        })
+
+
+
+    memespresent.print()
+    topmemeprchannel.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
+
+    /*
     val count = words.map(x => (x,1))
       .reduceByKeyAndWindow(_+_,_-_,Minutes(1),Seconds(3))
 
-    val total = count.reduce((t1,t2) => ("tot", t1._2 + t2._2))
+    val total = count.reduce((t1,t2) => (t1._1, t1._2 + t2._2))
     val numberTotal = total.reduceByKey((i1,i2) => i1 + i2 )
     numberTotal.foreachRDD(rdd =>{
       val i = rdd.take(1)(0)._2
@@ -60,7 +117,7 @@ object streaming {
 
 
 
-    val emotesinchat = count.filter(msg => memesbrodc.value.contains(msg._1.toLowerCase)).persist()
+    val emotesinchat = count.filter(msg => memesbrodc.value.contains(msg._1)).persist()
       emotesinchat.reduce((t1,t2) => ("meme", t1._2 + t2._2))
         .foreachRDD(rdd =>{
         val i = rdd.take(1)(0)._2
@@ -88,12 +145,10 @@ object streaming {
       }
     )
 
-    ssc.start()
-    ssc.awaitTermination()
-
-  }
 
 
 
 
-}
+
+*/
+
